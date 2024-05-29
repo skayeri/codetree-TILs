aec215ccbc793366dcd91929340af582810aeb8c
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-05-30 01:57:45 +0900

    Title: 문자열 나누기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240530/문자열 나누기/divide-string.py
+++ 240530/문자열 나누기/divide-string.py
@@ -5,12 +5,12 @@ s = ''
 for i in numbers:
     s += i
 
-curr = 1
-target = 5
+curr = 0
 
 for j in range(len(s)):
     curr += 1
-    if curr % target == 0:
+    if curr % 5 == 0:
         print(s[curr-5:curr])
+
 if (len(s) % 5) != 0:
     print(s[-(len(s) % 5):])
