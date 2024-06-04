0b8244b7964cd7096033d85b4e2c8d3ea8c0557c
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-04 20:38:59 +0900

    Title: 문자열 놀이, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240604/문자열 놀이/play-with-string.py
@@ -0,0 +1,20 @@
+s, q = input().split()
+q = int(q)
+q_list = []
+s_list = list(s)
+
+for _ in range(q):
+    q_list.append(input().split())
+
+for i in q_list:
+    if i[0] == '1':
+        t1 = s_list[int(i[1]) - 1]
+        t2 = s_list[int(i[2]) - 1]
+        s_list[int(i[1]) - 1] = t2
+        s_list[int(i[2]) - 1] = t1
+        print(''.join(s_list))
+    elif i[0] == '2':
+        for idx, j in enumerate(s_list):
+            if j == i[1]:
+                s_list[idx] = i[2]
+        print(''.join(s_list))
