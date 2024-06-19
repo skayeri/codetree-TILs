4d634b5676b3f5bb6ec78402698df05c73561bef
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-19 19:05:30 +0900

    Title: 홀수만 출력 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240619/홀수만 출력 2/output-only-odd-2.py
+++ 240619/홀수만 출력 2/output-only-odd-2.py
@@ -1,10 +1,5 @@
 b, a = map(int, input().split())
 l = []
 
-for i in range(a, b+1, 2):
-    l.append(i)
-
-l.reverse()
-
-for j in l:
-    print(j, end=' ')
+for i in range(b, a - 1, -2):
+    print(i, end=' ')
