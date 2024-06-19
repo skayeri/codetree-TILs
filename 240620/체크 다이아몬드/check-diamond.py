89658ead1bbc7c0abb013b3e0fb238febf611ce6
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-20 00:48:59 +0900

    Title: 체크 다이아몬드, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240620/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1, 2*n):
+    if i <= n:
+        print(' '*(n-i), end='')
+        print('* '*i)
+    else:
+        print(' '*(i-n), end='')
+        print('* '*(2*n-i))
