2e397a7c03c0becba453865bb0a15df88525a818
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-20 01:49:31 +0900

    Title: 미는 횟수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/미는 횟수/number-of-pushes.py
+++ 240620/미는 횟수/number-of-pushes.py
@@ -1,10 +1,12 @@
 a = input()
 b = input()
 n = 0
+ans = -1
 
 for i in range(len(a)):
     n += 1
     a = a[-1] + a[:-1]
     if a == b:
-        print(n)
+        ans = n
         break
+print(ans)
