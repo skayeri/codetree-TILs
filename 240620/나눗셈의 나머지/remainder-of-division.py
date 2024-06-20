846ac63115fd3bc4465af0fa26c558e17c13df43
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-20 20:09:38 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240620/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,19 @@
+a, b = map(int, input().split())
+l = []
+m = a
+answer = 0
+
+while m > 1:
+    l.append(int(m % b))
+    m /= b
+    continue
+
+arr = [0] * len(l)
+
+for i in l:
+    arr[i] += 1
+
+for j in arr:
+    answer += j**2
+
+print(answer)
