48b06a2c39fdf72cca359e63bf20b8f893396c54
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-26 20:42:06 +0900

    Title: 최소공배수 구하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240626/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,12 +1,14 @@
 def function(x, y):
     l = []
-    for i in range(1, x + 1):
-        a = x * i
-        for j in range(1, x + 1):
-            b = y * j
-            if a == b:
-                print(a)
-                exit(0)
+    k = []
+    for i in range(1, y + 1):
+        l.append(x * i)
+    for j in range(1, y + 1):
+        k.append(y * j)
+    for q in l:
+        if q in k:
+            print(q)
+            break
 
 n, m = map(int, input().split())
 
