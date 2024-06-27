2c9e79db748574b9ae3a92d2e78bb08864ec490a
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-06-27 13:55:51 +0900

    Title: 그 계절, 그 날, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/그 계절, 그 날/that-season-that-day.py
+++ 240627/그 계절, 그 날/that-season-that-day.py
@@ -1,15 +1,5 @@
 def season(y, m, d):
-    if m == 2:
-        if y % 4 == 0 and y % 100 == 0 and y % 400:
-            last_day = 29
-        elif y % 4 == 0 and y % 100 == 0:
-            last_day = 28
-        elif y % 4 == 0:
-            last_day = 29
-        else:
-            last_day = 28
-        ans = 'Winter'
-    elif 3 <= m <= 5:
+    if 3 <= m <= 5:
         ans = 'Spring'
     elif 6 <= m <= 8:
         ans = 'Summer'
@@ -17,10 +7,19 @@ def season(y, m, d):
         ans = 'Fall'
     else:
         ans = 'Winter'
-    
-    if m in [1, 3, 5, 7, 8, 10, 12]:
+
+    if m == 2:
+        if y % 4 == 0 and y % 100 == 0 and y % 400 == 0:
+            last_day = 29
+        elif y % 4 == 0 and y % 100 == 0:
+            last_day = 28
+        elif y % 4 == 0:
+            last_day = 29
+        else:
+            last_day = 28
+    elif m in [1, 3, 5, 7, 8, 10, 12]:
         last_day = 31
-    elif m in [4, 6, 9, 11]:
+    else:
         last_day = 30
 
     if d <= last_day:
