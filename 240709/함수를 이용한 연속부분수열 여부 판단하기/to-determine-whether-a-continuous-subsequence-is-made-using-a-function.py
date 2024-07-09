8f5cada4eac20ea7f9402fcac84167e3dcfa7582
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-07-09 22:55:10 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240709/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,19 @@
+n1, n2 = map(int, input().split())
+a = list(input().split())
+b = list(input().split())
+
+def cont(a, b):
+    start = 0
+    for i in range(n1):
+        if a[i] == b[0]:
+            start = i
+            break
+    temp = []
+    for j in range(n2):
+        temp.append(a[start + j])
+    if temp == b:
+        print('Yes')
+    else:
+        print('No')
+        
+cont(a, b)
