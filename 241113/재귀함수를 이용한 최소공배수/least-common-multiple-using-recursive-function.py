d2b4caa6c355249480f49c563a0caab80e465d50
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2024-11-13 00:02:42 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 45ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241113/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,21 @@
+# a, b의 최소공배수 : a * b / (a, b의 최대공약수)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def sol(arr, n):
+    if len(arr) == 1:
+        return arr[0]
+    if len(arr) == 2:
+        return lcm(arr[0], arr[1])
+    return lcm(arr[n-1], sol(arr[:n-1], n - 1))
+
+print(sol(arr, n))
