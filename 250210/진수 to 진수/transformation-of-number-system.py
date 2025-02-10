a6e23fca12b70847795818be4033b16c8a502b7a
Yerin Nam <89565895+skayeri@users.noreply.github.com>
2025-02-10 23:14:03 +0900

    Title: 진수 to 진수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250210/진수 to 진수/transformation-of-number-system.py
+++ 250210/진수 to 진수/transformation-of-number-system.py
@@ -3,3 +3,18 @@ n = input()
 
 nlist = list(map(int, n))
 
+num = 0
+for i in range(len(nlist)):
+    num = num * a + nlist[i]
+
+ans = []
+while True:
+    if num < b:
+        ans.append(num)
+        break
+    
+    ans.append(num % b)
+    num //= b
+
+for a in ans[::-1]:
+    print(a, end='')
